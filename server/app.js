08ac748ffcc671d93dc99e886c63395533608020
Lucas Ramon <lucasramonsa@gmail.com>
2018-05-17 21:26:04 -0300

    Terceira Parte

--- server/app.js
+++ server/app.js
@@ -28,10 +28,53 @@ app.get('/', function (req, res) {
   res.render('index', { players: db.players })
 })
 
+
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter umas 15 linhas de código
+function isEquals(jogador, id){
+	console.log(jogador.steamid, id)
+	return jogador.steamid === id;
+}
 
+app.get('/jogador/:numero_identificador/', function(req, res) {
+	var id = req.params.numero_identificador;
+	var players = db.players;
+	var gamesByPlayer = db.jogosPorJogador[id].games;
+	var numberOfGames = gamesByPlayer.length;
+	var notPlayed = 0, favorite, mostPlayed;
+	
+	
+	for(i=0; i<players.length; i++){
+		if(players[i].steamid == id){
+			var player = players[i];
+		}
+	}
+	
+	for(i=0; i < numberOfGames; i++){		
+		if(gamesByPlayer[i].playtime_forever == 0){
+			notPlayed++;
+		}
+	}
+
+	gamesByPlayer.sort(function(a, b) {
+		return b.playtime_forever - a.playtime_forever;
+	});
+
+	gamesByPlayer = gamesByPlayer.slice(0, 5);
+	gamesByPlayer = gamesByPlayer.map(x => {
+		x.playtime_forever = parseInt(x.playtime_forever / 60);
+		return x;
+	})
+
+	res.render('jogador', {
+		player: player,
+		gamesByPlayer: gamesByPlayer,
+		favorite: gamesByPlayer[0],
+		numberOfGames: numberOfGames,
+		notPlayed: notPlayed
+	});
+});
 
